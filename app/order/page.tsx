51562953bed7e0f691f34352c845d7bb65f7c5b8
Ahmad Saleem <ahmadsaleem19950@gmail.com>
2026-04-12 17:45:30 +0500

    ujdpate

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -162,7 +162,7 @@ export default function OrderPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-[#0A0A0A] flex flex-col">
+    <div className="min-h-screen bg-white dark:bg-black transition-colors duration-500">
       <Header />
       <main className="flex-1 container mx-auto px-4 py-6 md:py-10 pb-28 md:pb-10">
         {/* Page Header */}
